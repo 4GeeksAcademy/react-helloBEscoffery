44fb299535ba4407b380dd296bbd3aac1132b89f
Britney Escoffery <britneybescoffery@gmail.com>
2025-08-16 00:58:25 +0000

    added button

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [toDoList,setToDoList] = useState(["Clean Room", "Mop Kitchen", "Walk the Dog", "Dust"]);
+	const nextTask = ()=>{
+		setInputValue([toDoList])
+	}
+	
 	return (
 		<div className="text-center">
-            
+         <input onChange={ (event)=> { 
+			const newTask = event.target.value
+			// console.log(event.target.value)
+			setInputValue(newTask)} //hook
+		 } />
+		<button onClick={()=> nextTask()}>
+			Add task to list
+		</button>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		 <ul>
+			{/* <li>Take out the trash</li> */}
+			{toDoList.map(
+				(item, index)=> {
+					return(
+							<li key = {index + "chore"}>{item}</li>
+				)
+			}
+			)}
+		 </ul>
+		 {inputValue}
 		</div>
 	);
 };
